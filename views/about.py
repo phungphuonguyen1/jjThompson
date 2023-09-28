c3e87f02c4127642ddbd48bbab168afbee807883
phungphuonguyen1 <21010343@st.phenikaa-uni.edu.vn>
2023-09-28 18:41:42 +0700

    float integer

--- views/about.py
+++ views/about.py
@@ -3,7 +3,7 @@ from def_file import *
 def user_input_features():
     Voltage=st.slider('Voltage: ',0.0,10.0, 3.0)
     Distance=st.slider('Distance: ',0.01,0.1, 0.05,step=0.01)
-    B_field=st.slider('10^6*B_field: ',1,10,step=0.1)
+    B_field=st.slider('10^6*B_field: ',1.0,10.0,step=0.1)
     X1=st.slider('x1: ',0.1,1.0,0.2,step=0.01)
     X2=st.slider('x2: ',0.1,1.0,0.2,step=0.01)
     features=[float(Voltage),float(Distance),float(X1),float(X2),float(B_field/10^6)]
