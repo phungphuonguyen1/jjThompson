b9f6e43a7e1356720aa3878ad11b1eceb4488a2b
phungphuonguyen1 <21010343@st.phenikaa-uni.edu.vn>
2023-09-28 19:04:38 +0700

    style

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import utils as utl
 from views import home,about,analysis,options,configuration
 
-st.set_page_config(layout="wide", page_title='Navbar sample')
+st.set_page_config(layout="wide", page_title='JJ Thomson')
 st.set_option('deprecation.showPyplotGlobalUse', False)
 utl.inject_custom_css()
 utl.navbar_component()
