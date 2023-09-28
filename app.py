4f2d4151cfa7a6d0043298cac6aed68e19bbcdbb
phungphuonguyen1 <21010343@st.phenikaa-uni.edu.vn>
2023-09-28 17:56:06 +0700

    bỏ mô phỏng ở sidebar

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ with st.expander("Input explaination"):
 
 input = user_input_features()
 st.button("hey this is button")
-
+#
 if st.button("Mô phỏng"):
     df=path_function(input[0],input[1],input[2],input[3],input[4])
     #path
