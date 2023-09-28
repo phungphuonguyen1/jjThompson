c46e8e18a75cd2e70885bba50ad5328766c1be51
phungphuonguyen1 <21010343@st.phenikaa-uni.edu.vn>
2023-09-29 01:26:00 +0700

    Update home.py

--- views/home.py
+++ views/home.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 def load_view():
-    st.markdown('Thí nghiệm đo %\dfrac{e}{m}$ của J.J. Thomson',unsafe_allow_html=True)
+    st.markdown('Thí nghiệm đo $\dfrac{e}{m}$ của J.J. Thomson',unsafe_allow_html=True)
