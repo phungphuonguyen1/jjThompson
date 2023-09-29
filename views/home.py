62324daf5b998053adeb09ef3a47ae4d4ca06540
phungphuonguyen1 <114821616+phungphuonguyen1@users.noreply.github.com>
2023-09-29 16:29:03 +0700

    Update home.py

--- views/home.py
+++ views/home.py
@@ -9,7 +9,7 @@ def load_view():
     #image_file= Image.open("assets/images/gettyimages-113634985.jpg")
     #st.image(image_file)
     #img=Image.open("/assets/images/gettyimages-113634985.jpg")
-    pa1=''' <p class='styled-paragraph'>
+    pa1="""<p class='styled-paragraph'>
                 Joseph John Thomson là nhà Vật lý học người Anh, người đã vinh dự nhận giải Nobel Vật Lý vào năm 1906.<br>Vào năm `897, Thomson đã chỉ ra rằng các tia phát ra từ cathode được tạo bởi
                 các hạt chưa biết mang điện tích âm (ngày nay, chúng ta gọi nó là electron). Với các hạt này, ông đã tính được rằng kích thước của chúng là rất nhỏ so với kích thước 1 nguyên tử và
                 tỷ lệ giữa điện tích và khối lượng của hạt này là rất lớn.<br>Thomson cũng được biết tới là người đầu tiên đưa ra bằng chứng về các đồng vị của một nguyên tố bền (không phóng xạ) 
@@ -17,3 +17,5 @@ def load_view():
                 dẫn tới sự phát triển của máy quang phổ khối.<br>Thomson đã được trao giải Nobel Vật lý vào năm 1906 với công trình về sự dẫn điện trong chất khí. Thomson cũng là một giáo viên,
                 có 9 người trong số các học sinh, đồng nghiệp và cấp dưới (bao gồm cả con trai ông) cũng giành được giải Nobel.
             </p>
+        """
+    st.markdown(pa1,unsafe_allow_html=True)
