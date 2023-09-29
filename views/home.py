d037a4b46914467b28f2669b35a17c4faa7644cd
phungphuonguyen1 <114821616+phungphuonguyen1@users.noreply.github.com>
2023-09-29 15:55:28 +0700

    Update home.py

--- views/home.py
+++ views/home.py
@@ -1,5 +1,9 @@
 import streamlit as st
-
+from PIL import Image
 def load_view():
-    title=<h1>Thí nghiệm đo $\dfrac{e}{m}$ của J.J. Thomson</h1>
-    st.markdown('Thí nghiệm đo $\dfrac{e}{m}$ của J.J. Thomson',unsafe_allow_html=True)
+    title_main='<h1 style="text-align:center; font-weight: bolder;color: #EE9322;text-shadow: 3px 1px blue;">GIỚI THIỆU CHUNG</h1>'
+    st.markdown(title_main,unsafe_allow_html=True)
+    #st.markdown('Thí nghiệm đo $\dfrac{e}{m}$ của J.J. Thomson',unsafe_allow_html=True)
+    head2='<h2 style="text-align:left; font-weight: bolder;color: #FCF3CF;text-shadow: 3px 1px black;">J.J. Thomson</h2>'
+    st.markdown(head2,unsafe_allow_html=True)
+    img=Image.open("https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/J.J_Thomson.jpg/300px-J.J_Thomson.jpg")
