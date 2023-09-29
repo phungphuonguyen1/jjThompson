2fe961cad50f9f74ce6f70278c8bf7a452427228
phungphuonguyen1 <114821616+phungphuonguyen1@users.noreply.github.com>
2023-09-29 17:17:20 +0700

    Update home.py

--- views/home.py
+++ views/home.py
@@ -5,32 +5,31 @@ def load_view():
     st.markdown(title_main,unsafe_allow_html=True)
 
     #table of content
-    menu='<div class="toc">
-        <h2>Table of Contents</h2>
+    menu="""<div class="toc">
+        <h2 class='styled-heading'>Table of Contents</h2>
         <ul>
             <li><a href="#section1">1. J.J. Thomson</a></li>
             <li><a href="#section2">Section 2</a></li>
         </ul>
-    </div>'
+    </div>"""
     st.markdown(menu,unsafe_allow_html=True)
 
-    ses1= ' <div id="section1">
+    ses1= """<div id="section1">
         <h2 class="styled-heading">1. J.J. Thomson</h2>
-        <img src="assets/images/gettyimages-113634985.jpg">
+        #<img src="assets/images/gettyimages-113634985.jpg">
         <p class='styled-paragraph'>
                 Joseph John Thomson là nhà Vật lý học người Anh, người đã vinh dự nhận giải Nobel Vật Lý vào năm 1906.<br>Vào năm `897, Thomson đã chỉ ra rằng các tia phát ra từ cathode được tạo bởi
                 các hạt chưa biết mang điện tích âm (ngày nay, chúng ta gọi nó là electron). Với các hạt này, ông đã tính được rằng kích thước của chúng là rất nhỏ so với kích thước 1 nguyên tử và
                 tỷ lệ giữa điện tích và khối lượng của hạt này là rất lớn.<br>Thomson cũng được biết tới là người đầu tiên đưa ra bằng chứng về các đồng vị của một nguyên tố bền (không phóng xạ) 
                 vào năm 1913. Các thí nghiệm của ông nhằm xác định bản chất của các hạt tích điện dương, cùng với nhà Vật lý học Francis William Aston, là ứng dụng đầu tiên của phép đo khối phổ và
                 dẫn tới sự phát triển của máy quang phổ khối.<br>Thomson đã được trao giải Nobel Vật lý vào năm 1906 với công trình về sự dẫn điện trong chất khí. Thomson cũng là một giáo viên,
-                có 9 người trong số các học sinh, đồng nghiệp và cấp dưới (bao gồm cả con trai ông) cũng giành được giải Nobel.
-        </p>
-        </div>'
+                có 9 người trong số các học sinh, đồng nghiệp và cấp dưới (bao gồm cả con trai ông) cũng giành được giải Nobel.</p>
+        </div>"""
     st.markdown(ses1,unsafe_allow_html=True)
-    ses2='<div id="section2">
+    ses2="""<div id="section2">
             <h2 class="styled-heading">2. Thí nghiệm tìm ra tỷ số e/m</h2>
             <p class="styled-paragraph">Thí nghiệm này được tiến hành vào năm 1897</p>
-        </div>'
+        </div>"""
     st.markdown(ses2,unsafe_allow_html=True)
     
     
