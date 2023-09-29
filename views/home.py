81e0433eaa1e4509bef87aa9b4698d16373d2f57
phungphuonguyen1 <114821616+phungphuonguyen1@users.noreply.github.com>
2023-09-29 16:04:09 +0700

    Update home.py

--- views/home.py
+++ views/home.py
@@ -6,6 +6,6 @@ def load_view():
     #st.markdown('Thí nghiệm đo $\dfrac{e}{m}$ của J.J. Thomson',unsafe_allow_html=True)
     head2='<h2 style="text-align:left; font-weight: bolder;color: #FCF3CF;text-shadow: 3px 1px black;">J.J. Thomson</h2>'
     st.markdown(head2,unsafe_allow_html=True)
-    image_file= open("assets/images/gettyimages-113634985.jpg", "rb")
+    image_file= Image.open("assets/images/gettyimages-113634985.jpg")
     st.image(image_file)
     #img=Image.open("/assets/images/gettyimages-113634985.jpg")
