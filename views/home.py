33c6afb5c71dbfd73ad013fbb880c2b0bce7c2c1
phungphuonguyen1 <114821616+phungphuonguyen1@users.noreply.github.com>
2023-09-30 01:32:35 +0700

    Update home.py

--- views/home.py
+++ views/home.py
@@ -9,14 +9,15 @@ def load_view():
         <h2 class='styled-heading'>Table of Contents</h2>
         <ul>
             <li><a href="#section1">1. J.J. Thomson</a></li>
-            <li><a href="#section2">Section 2</a></li>
+            <li><a href="#section2">2. Thí nghiệm tìm ra tỷ số e/m</a></li>
+            <li><a href="#section3">3. Tài liệu tham khảo</a></li>
         </ul>
     </div>"""
     st.markdown(menu,unsafe_allow_html=True)
 
     ses1= """<div id="section1">
         <h2 class="styled-heading">1. J.J. Thomson</h2>
-        <img src="assets/images/gettyimages-113634985.jpg">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/J.J_Thomson.jpg/250px-J.J_Thomson.jpg">
         <p class='styled-paragraph'>
                 Joseph John Thomson là nhà Vật lý học người Anh, người đã vinh dự nhận giải Nobel Vật Lý vào năm 1906.<br>Vào năm `897, Thomson đã chỉ ra rằng các tia phát ra từ cathode được tạo bởi
                 các hạt chưa biết mang điện tích âm (ngày nay, chúng ta gọi nó là electron). Với các hạt này, ông đã tính được rằng kích thước của chúng là rất nhỏ so với kích thước 1 nguyên tử và
@@ -32,5 +33,14 @@ def load_view():
         </div>"""
     st.markdown(ses2,unsafe_allow_html=True)
     
+    ses3="""
+        <div id="section3">
+            <h2 class="styled-heading">3. Tài liệu liên quan</h2>
+            <a href="https://youtu.be/Rb6MguN0Uj4">Video giải thích thí nghiệm</a>
+            <a href="https://youtu.be/_nLESblUAHY">Video mô phỏng thí nghiệm</a>
+        </div>      
+        """
+    st.markdown(ses3,unsafe_allow_html=True)
+
     
 
