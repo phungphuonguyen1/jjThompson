d6818cde612634e730d6af6d5b7337f6d4422384
phungphuonguyen1 <114821616+phungphuonguyen1@users.noreply.github.com>
2023-10-02 00:31:14 +0700

    Update calculator.py

--- views/calculator.py
+++ views/calculator.py
@@ -50,6 +50,17 @@ def load_view():
         st.markdown(ls1,unsafe_allow_html=True)
 
     col1,col2= st.columns([1,1])
+    col12= """<div class="container-author">
+            <div class="text">
+                input = user_input_features()
+            </div>
+            <div class="image">
+            <h2 class="styled-heading">Hình vẽ mô phỏng thí nghiệm thực tế</h2>'
+            <img src="Specific-Charge-Ratio-03.png">
+            </div>
+    </div>"""
+    st.markdown(col12,unsafe_allow_html=True)
+    
     with col1: 
         input = user_input_features()
     with col2:
