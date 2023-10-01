55d3b3e472703220b2d26532690cf07914f976c4
phungphuonguyen1 <114821616+phungphuonguyen1@users.noreply.github.com>
2023-10-02 00:50:08 +0700

    Update home.py

--- views/home.py
+++ views/home.py
@@ -62,7 +62,6 @@ def load_view():
         </div>      
         """
     st.markdown(ses3,unsafe_allow_html=True)
-    st.video("https://youtu.be/Rb6MguN0Uj4")
-
+    st_player("https://youtu.be/Rb6MguN0Uj4")
     
 
