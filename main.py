bd93ff3bcef2a3e182741b934ae82ab5015c6575
phungphuonguyen1 <114821616+phungphuonguyen1@users.noreply.github.com>
2024-02-21 11:35:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,6 @@
 import streamlit as st
 import utils as utl
 from views import home,about_me,calculator,options,configuration
-from PATHS import NAVBAR_PATHS, SETTINGS
-# Add CSS styles for the navigation bar
-st.markdown(
-    """
-    <style>
-        .navbar {
-            font-family: 'Your Desired Font', sans-serif;
-            background-color: #3498db;  /* Change the background color as needed */
-            padding: 10px;
-            color: #ffffff;  /* Change the text color as needed */
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-# Create the navigation bar
-selected_option = st.sidebar.radio("Navigation", list(NAVBAR_PATHS.keys()))
-
-# Display content based on the selected option
-if selected_option in NAVBAR_PATHS:
-    st.write(f"Selected option: {selected_option}")
-    # Add logic to display content based on the selected option
-
-elif selected_option in SETTINGS:
-    st.write(f"Selected settings: {selected_option}")
-    # Add logic to display content based on the selected setting
-
-st.set_page_config(layout="wide", page_title='JJ Thomson',page_icon='🤓')
-st.set_option('deprecation.showPyplotGlobalUse', False)
-utl.inject_custom_css()
-utl.navbar_component()
 
 # hide main menu and footer
 hide_menu_style= """
