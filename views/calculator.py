cd0ea67e114356796735d3052ba01df76b59afec
phungphuonguyen1 <114821616+phungphuonguyen1@users.noreply.github.com>
2024-02-21 14:31:39 +0700

    Update calculator.py

--- views/calculator.py
+++ views/calculator.py
@@ -39,13 +39,15 @@ def load_view():
         ''',
         unsafe_allow_html=True)
     # input explaination:
-    ls1= r"""<ul>
-            <li>V: Điện thế của bản tụ (V)</li>
-            <li>D: Khoảng cách giữa 2 bản tụ (m)</li>
-            <li>B_field: Độ lớn cường độ điện trường (T)</li>
-            <li> $l_1$: Chiều dài bản tụ (m)</li>
-            <li> $l_2$: Khoảng cách từ bản tụ đến màn chắn (m)</li>
-        </ul>"""
+    ls1 = r"""
+    \begin{itemize}
+        \item $V$: Điện thế của bản tụ (V)
+        \item $D$: Khoảng cách giữa 2 bản tụ (m)
+        \item $B_{\text{field}}$: Độ lớn cường độ điện trường (T)
+        \item $l_1$: Chiều dài bản tụ (m)
+        \item $l_2$: Khoảng cách từ bản tụ đến màn chắn (m)
+    \end{itemize}
+    """
     st.write("Giải thích các thông số đầu vào:")     
     st.markdown(ls1,unsafe_allow_html=True)
 
