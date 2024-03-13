77c85167937ded6e1165cce3624f2d1a909a3c06
phungphuonguyen1 <114821616+phungphuonguyen1@users.noreply.github.com>
2024-03-13 14:15:06 +0700

    Update calculator.py

--- views/calculator.py
+++ views/calculator.py
@@ -64,9 +64,12 @@ def load_view():
         st.markdown(m,unsafe_allow_html=True)
         '''hed23='<h2 class="styled-heading">Cơ sở lý thuyết cho tính toán</h2>'
         st.markdown(hed23,unsafe_allow_html=True)"""
+        m2= """
         <a href="https://drive.google.com/file/d/17eAjIquLkHxjOCgGHPe77aZjhoZh6F6r/view?usp=sharing">Bản Tiếng Anh</a>
         <br>
         <a href="https://drive.google.com/file/d/1mtRizC2kHO4K26WUaQpjHwMuLQPqBCCs/view?usp=sharing">Bản Tiếng Việt</a>
+        """
+        st.markdown(m2,unsafe_allow_html=True)
         
     col3,col4=st.columns([2,1])
     if col1.button("Mô phỏng"):
