e30372ea770193d0ccdabef371dc41e29a79daa4
phungphuonguyen1 <114821616+phungphuonguyen1@users.noreply.github.com>
2024-03-13 13:42:32 +0700

    Update calculator.py

--- views/calculator.py
+++ views/calculator.py
@@ -98,10 +98,10 @@ def load_view():
             #facilities
             ax.hlines(y=D/2, xmin=0, xmax=x1, linewidth=4, color='black')
             ax.hlines(y=-D/2, xmin=0, xmax=x1, linewidth=4, color='black')
-            ax.text(0.95*x1/2,(1.1*D)/2,"+")
+            ax.text(0.9*x1/2,(1.1*D)/2,"+")
             ax.vlines(x=x1+x2, ymin=-D/2, ymax=1.5*max((y1+y2),D/2), linewidth=3, color='black')
             ax.hlines(y=0, xmin=0, xmax=x1+x2, linestyles='dotted',color='black')
-            ax.text(0.95*x1/2,(-1.1*D)/2,"-")
+            ax.text(0.9*x1/2,(-1.1*D)/2,"_")
             
             #distance note
             # x1
