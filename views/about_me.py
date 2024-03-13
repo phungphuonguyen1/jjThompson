9a6d032ff2ad64c3553775474b0ae8c3673f064e
phungphuonguyen1 <114821616+phungphuonguyen1@users.noreply.github.com>
2024-03-13 14:28:56 +0700

    Update about_me.py

--- views/about_me.py
+++ views/about_me.py
@@ -1,5 +1,7 @@
 import streamlit as st
     
 def load_view():
-    st.title('Thông tin nhóm thực hiện')
+    title_main='<h1 style="text-align:center; font-weight: bolder;color: #246BA6 ;text-shadow: 3px 1px #83C7FF ;">Thông tin nhóm thực hiện</h1>'
+
+    st.markdown(title_main,unsafe_allow_html=True)
     
