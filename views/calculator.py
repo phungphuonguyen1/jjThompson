4dbd6f1538d85e2876a90bcbff34ae428acc50eb
phungphuonguyen1 <114821616+phungphuonguyen1@users.noreply.github.com>
2024-03-13 13:49:03 +0700

    Update calculator.py

--- views/calculator.py
+++ views/calculator.py
@@ -18,7 +18,7 @@ def user_input_features():
 
 def load_view():    
     title_main='''<h1 class="styled-heading">
-        Chương trình mô phỏng thí nghiệm đo tỷ sổ khối lượng/ điện tích của J. J. Thompson
+        Chương trình mô phỏng thí nghiệm đo tỷ sổ khối lượng/ điện tích của J. J. Thomson
     </h1>'''
     st.markdown(title_main,unsafe_allow_html=True)
     
